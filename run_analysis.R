cdf5f24ad2b4bbdd7dfcabd8a5551d031c57a2cb
pulkitkalia1994 <pulkit.kalia1994@gmail.com>
2018-05-19 10:01:11 +0530

    Added missing subject column and changed grouping

--- run_analysis.R
+++ run_analysis.R
@@ -1,21 +1,19 @@
 run_analysis<-function(){
- ##collecting data
+  ##collecting data
   X_train<-read.table(".\\UCI HAR Dataset\\train\\X_train.txt")
   X_test<-read.table(".\\UCI HAR Dataset\\test\\X_test.txt")
   ##collecting names of the data
   names<-read.table(".\\UCI HAR Dataset\\features.txt")
   names<-names[,2]
 
-  ##merging data
   part1<- rbind(X_train,X_test)
   names(part1)<-names
-  
-  ##choosing only mean/std columns
+  ##extracting only columns with mean/std
   particularNames<-grep("mean|std",names)
   particularNames<-names[particularNames]
-	
-  ##adding activity related data	
+  
   part2<-part1[,particularNames]
+  ##extracting activity and merging
   activity1<-read.table(".\\UCI HAR Dataset\\train\\y_train.txt")
   activity2<-read.table(".\\UCI HAR Dataset\\test\\y_test.txt")
   totalActivity<-rbind(activity1,activity2)
@@ -28,10 +26,13 @@ run_analysis<-function(){
     activityDescription[i]<-switch(val,"WALKING","WALKING_UPSTAIRS","WALKING_DOWNSTAIRS","SITTING","STANDING","LAYING")
     i<-i+1
   }
-  ##creating a data set with activityId and activityDescription
-  part4<-mutate(part2,activityId=totalActivity,activityDescription=activityDescription)
-  
+  ##extracting subject and merging
+  subject_train<-read.table(".\\UCI HAR Dataset\\train\\subject_train.txt")
+  subject_test<-read.table(".\\UCI HAR Dataset\\test\\subject_test.txt")
+  subject<-rbind(subject_train,subject_test)$V1
+  ##creating a data set with activityId, activityDescription and subject
+  part4<-mutate(part2,activityId=totalActivity,activityDescription=activityDescription,subject=subject)
   ##grouping them with the column names and summarising them with their mean
-  part5<-part4 %>% group_by_all() %>% summarise_all(mean)
-  write.table(part5, file = "./finalData.txt")
+  part5<-part4 %>% group_by(activityDescription,subject) %>% summarise_all(funs(mean))
+  write.table(part5, file = "./tidyData.txt")
 }
